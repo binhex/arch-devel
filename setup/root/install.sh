3254b0e6f65a3f6ebda6804b285bee6ee4e35303
binhex <megalith01@gmail.com>
2016-11-02 13:23:09 +0000

    use var name for helper

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -9,9 +9,12 @@ pacman_packages="base-devel"
 # install pre-reqs
 pacman -S --needed $pacman_packages --noconfirm
 
+# define aur helper
+aur_helper="apacman"
+
 # manually download aur helper from binhex repo
-curl -o /tmp/apacman-3.0-1-any.pkg.tar.xz -L https://github.com/binhex/arch-packages/raw/master/compiled/apacman-3.0-1-any.pkg.tar.xz
-pacman -U /tmp/apacman-3.0-1-any.pkg.tar.xz --noconfirm
+curl -o "/tmp/${aur_helper}-any.pkg.tar.xz" -L "https://github.com/binhex/arch-packages/raw/master/compiled/${aur_helper}-any.pkg.tar.xz"
+pacman -U "/tmp/${aur_helper}-any.pkg.tar.xz" --noconfirm
 
 # cleanup
 yes|pacman -Scc
