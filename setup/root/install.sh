12d56f63d13f128e35fab11850583bf667d99a72
binhex <megalith01@gmail.com>
2016-11-08 17:00:20 +0000

    update base, refactor install

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -3,14 +3,8 @@
 # exit script if return code != 0
 set -e
 
-# define pacman packages
-pacman_packages="base-devel"
-
-# define aur helper
-aur_helper="apacman"
-
-# install pre-reqs
-pacman -S --needed $pacman_packages --noconfirm
+# build scripts
+####
 
 # download build scripts from github
 curl -o /tmp/scripts-master.zip -L https://github.com/binhex/scripts/archive/master.zip
@@ -21,8 +15,43 @@ unzip /tmp/scripts-master.zip -d /tmp
 # move shell scripts to /root
 find /tmp/scripts-master/ -type f -name '*.sh' -exec mv -i {} /root/  \;
 
+# pacman packages
+####
+
+# define pacman packages
+pacman_packages="base-devel"
+
+# install compiled packages using pacman
+if [[ ! -z "${pacman_packages}" ]]; then
+	pacman -S --needed $pacman_packages --noconfirm
+fi
+
+# aor packages
+####
+
+# define arch official repo (aor) packages
+aor_packages=""
+
+# call aor script (arch official repo)
+source /root/aor.sh
+
+# aur packages
+####
+
+# define aur helper
+aur_helper="apacman"
+
+# define aur packages
+aur_packages=""
+
 # call aur install script (arch user repo)
 source /root/aur.sh
+
+# container perms
+####
+
+# env vars
+####
 
 # cleanup
 yes|pacman -Scc
