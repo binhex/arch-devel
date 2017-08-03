5128aadf82a89f8ec36c1285c22f843fcdbabf04
binhex <megalith01@gmail.com>
2017-08-03 20:43:50 +0100

    no confirm for deps

--- build/root/custom.sh
+++ build/root/custom.sh
@@ -7,6 +7,6 @@ set -e
 /usr/bin/python2 -m pip install pex virtualenv requests
 
 # bootstrap and install apacman
-pacman -S --needed --asdeps jshon
+pacman -S --needed --asdeps jshon --noconfirm
 curl -O "https://raw.githubusercontent.com/oshazard/apacman/master/apacman"
 bash ./apacman -S apacman --noconfirm
