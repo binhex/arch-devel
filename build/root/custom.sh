eb91a02315ba348bb9932e7ae96f20a16e6dc49c
binhex <megalith01@gmail.com>
2017-08-03 20:31:32 +0100

    add in apacman

--- build/root/custom.sh
+++ build/root/custom.sh
@@ -5,3 +5,8 @@ set -e
 
 # install additonal python modules
 /usr/bin/python2 -m pip install pex virtualenv requests
+
+# bootstrap and install apacman
+pacman -S --needed --asdeps jshon
+curl -O "https://raw.githubusercontent.com/oshazard/apacman/master/apacman"
+bash ./apacman -S apacman
