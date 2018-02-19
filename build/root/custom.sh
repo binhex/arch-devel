68e50ce5891a01cec43a374c3e977c29861d7d65
binhex <megalith01@gmail.com>
2018-02-19 13:09:13 +0000

    switch to pre-built apacman

--- build/root/custom.sh
+++ build/root/custom.sh
@@ -6,7 +6,12 @@ set -e
 # install additional python modules
 /usr/bin/python2 -m pip install pex virtualenv requests
 
-# bootstrap and install apacman
-pacman -S --needed --asdeps jshon --noconfirm
-curl -O "https://raw.githubusercontent.com/oshazard/apacman/master/apacman"
-bash ./apacman -S apacman --noconfirm
+# define aur helper and ver
+aur_helper="apacman"
+aur_helper_version="3.1-1"
+
+# install aur helper
+pacman -S --needed base-devel --noconfirm
+/root/curly.sh -rc 6 -rw 10 -of "/tmp/${aur_helper}-any.pkg.tar.xz" -url "https://github.com/binhex/arch-packages/raw/master/compiled/${aur_helper}-${aur_helper_version}-any.pkg.tar.xz"
+pacman -U "/tmp/${aur_helper}-any.pkg.tar.xz" --noconfirm
+
