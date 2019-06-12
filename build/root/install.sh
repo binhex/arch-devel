600804ad084e8c4f9dfd54ca27225a400047ac85
binhex <megalith01@gmail.com>
2019-06-12 21:15:57 +0100

    remove orphan packages and config

--- build/root/install.sh
+++ build/root/install.sh
@@ -49,6 +49,7 @@ source /root/aur.sh
 
 # cleanup
 yes|pacman -Scc
+pacman --noconfirm -Rns $(pacman -Qtdq) 2> /dev/null || true
 rm -rf /usr/share/locale/*
 rm -rf /usr/share/man/*
 rm -rf /usr/share/gtk-doc/*
