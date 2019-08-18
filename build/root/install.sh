266fc90501f78e8f61d3aae1c87b13cf311f41ff
binhex <megalith01@gmail.com>
2019-08-18 21:39:55 +0100

    use cleanup.sh script

--- build/root/install.sh
+++ build/root/install.sh
@@ -48,9 +48,4 @@ source aur.sh
 ####
 
 # cleanup
-yes|pacman -Scc
-pacman --noconfirm -Rns $(pacman -Qtdq) 2> /dev/null || true
-rm -rf /usr/share/locale/*
-rm -rf /usr/share/man/*
-rm -rf /usr/share/gtk-doc/*
-rm -rf /tmp/*
+cleanup.sh
